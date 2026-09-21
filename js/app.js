e6d089c9d082dc2a32da42f58d48f9acafc7f3fa
ameliaeberle6-hue <amelia.eberle6@gmail.com>
2026-09-21 10:30:49 -0500

    lab 13

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('app.js connected');
+let myFavorite = {
+    name: 'Starbucks on University Drive',
+    category: 'coffee',
+    rating: 5,
+    notes: 'Great study spot with fast wifi',
+    dateAdded: new Date().toLocaleDateString()
+};
+
+console.log(myFavorite);
+
+let displayText = myFavorite.name + ' - Rating: ' + myFavorite.rating + '/5';
+console.log(displayText);
+
+console.log(typeof myFavorite.name);
+console.log(typeof myFavorite.category);
+console.log(typeof myFavorite.rating);
+console.log(typeof myFavorite.notes);
+console.log(typeof myFavorite.dateAdded);
+
+let placeName = 'Starbucks';
+let rating = 5;
+console.log(placeName + ' - ' + rating + '/5');
+console.log('⭐'.repeat(rating) + ' ' + placeName);
