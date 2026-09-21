b924aeaa36eede8436c8c6d0e3b0be6c14a6b7f5
ameliaeberle6-hue <amelia.eberle6@gmail.com>
2026-09-21 11:34:36 -0500

    js. ampersand

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-let console.log('app.js connected');
-
-myFavorite = {
+let myFavorite = {
     name: 'Starbucks on University Drive',
     category: 'coffee',
     rating: 5,
     notes: 'Great study spot with fast wifi',
     dateAdded: new Date().toLocaleDateString()
 };
+
+let today = new Date().toLocaleDateString();
+console.log(today);
 
 console.log(myFavorite);
 
@@ -19,7 +20,24 @@ console.log(typeof myFavorite.rating);
 console.log(typeof myFavorite.notes);
 console.log(typeof myFavorite.dateAdded);
 
-let placeName = 'Starbucks';
+let placeName = 'Ampersand';
 let rating = 5;
 console.log(placeName + ' - ' + rating + '/5');
 console.log('⭐'.repeat(rating) + ' ' + placeName);
+
+function greetFavorite(placeName, rating) {
+    console.log(placeName + ' has ' + rating + ' stars!');
+}
+greetFavorite('Starbucks', 5);
+
+const nameInput = document.getElementById('name');
+console.log(nameInput.value);
+
+const practiceForm = document.getElementById('add-favorite-form');
+
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log('You typed: ' + nameInput.value);
+}
+
+practiceForm.addEventListener('submit', handleSubmit);
